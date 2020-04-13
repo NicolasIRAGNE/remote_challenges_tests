2e05f61d92ee93b2e46953cded27f88aa98bb76f
nicolasiragne <nicoragne@hotmail.fr>
2020-04-13 22:07:32 +0200

    updated for chall01 (grosse blague)

--- chall01/benchmark/benchmark.c
+++ chall01/benchmark/benchmark.c
@@ -6,7 +6,7 @@
 /*   By: niragne <niragne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 11:46:58 by niragne           #+#    #+#             */
-/*   Updated: 2020/04/12 15:25:44 by niragne          ###   ########.fr       */
+/*   Updated: 2020/04/13 22:02:38 by niragne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,25 @@
 # define MEDIUM_TEST_SIZE USHRT_MAX
 #endif
 
-int ft_necklace(char *s1, char *s2);
+char *ft_rgb2hex(int r, int g, int b);
 
 struct test_s
 {
-	char*	s1;
-	char*	s2;
-	int			ret;
+	int r;
+	int g;
+	int b;
+	char*	ret;
 };
 
 struct test_s easy[] =
 {
-	{"salut", "bonsoir", 0},
-	{"nicole", "icolen", 1},
-	{"nicole", "lenico", 1},
-	{"nicole", "coneli", 0},
-	{"aabaaaaabaab", "aabaabaabaaa", 1},
-	{"abc", "cba", 0},
-	{"xxyyy", "xxxyy", 0},
-	{"xyxxz", "xxyxz", 0},
-	{"x", "x", 1},
-	{"x", "xx", 0},
-	{"x", "", 0},
-	{":)", "):", 1},
-	{"", "s", 0},
+	{0, 0, 0, "#000000"},
+	{0, 1, 0, "#000100"},
+	{0xff, 0xff, 0xff, "#ffffff"},
+	{0x00, 0xff, 0xff, "#00ffff"},
+	{0xff, 0x00, 0xff, "#ff00ff"},
+	{0xff, 0xff, 0x00, "#ffff00"},
+	{0x0a, 0x0a, 0x0a, "#0a0a0a"},
 };
 
 int	easy_tests(void)
@@ -64,14 +59,15 @@ int	easy_tests(void)
 	int i = 0;
 	while (i < (sizeof(easy) / sizeof(struct test_s)))
 	{
-		if (ft_necklace(easy[i].s1, easy[i].s2) != easy[i].ret)
+		char* ret = ft_rgb2hex(easy[i].r, easy[i].g, easy[i].b);
+		if (strcmp(ret, easy[i].ret))
 		{
 			failed++;
-			printf("Test %d: KO\n", i);
+			printf("Test %d: KO (expected %s)\n", i, easy[i].ret, ret);
 		}
 		else
 			printf("Test %d: OK\n", i);
-		
+		free(ret);
 		i++;
 	}
 
@@ -89,8 +85,6 @@ int	medium_tests(void)
 {
 	int i = 0;
 	int failed = 0;
-	char	s1[MEDIUM_TEST_SIZE];
-	char	s2[MEDIUM_TEST_SIZE];
 
 	clock_t start;
 	clock_t stop;
@@ -98,26 +92,12 @@ int	medium_tests(void)
 	clock_t total_time = 0;
 	double seconds;
 
-	memset(s1, 'A', sizeof(s1));
-	memset(s2, 'A', sizeof(s2));
-	s1[sizeof(s1) - 1] = 0;
-	s2[sizeof(s2) - 1] = 0;
-
 	while (i < MEDIUM_TEST_LENGTH)
 	{
-		int index1 = rand() % sizeof(s1 - 1);
-		int index2 = rand() % sizeof(s2 - 1);
-		s1[index1] = 'B';
-		s2[index2] = 'B';
-		
 		start = clock();
-		if (ft_necklace(s1, s2) != 1)
-			failed++;
 		stop = clock();
 		diff = stop - start;
 		total_time += diff;
-		s1[index1] = 'A';
-		s2[index2] = 'A';
 		i++;
 	}
 	seconds = (float)total_time / CLOCKS_PER_SEC;
@@ -136,9 +116,6 @@ int main(int ac, char** av)
 	// srand(time(NULL));
 	printf("EASY TESTS:\n");
 	ret += easy_tests();
-	printf("\n");
-	printf("MEDIUM TESTS:\n");
-	ret += medium_tests();
 	printf("\n");
 	return (ret);
 }
